c85344e7bfc489d35f2e2747e0a8241107e090f8
konark1999 <kitugarg.199@gmail.com>
2021-06-17 10:11:57 +0530

    Changes committed: 	added 4 arguments function 	refactored java code

--- CCode/addition.c
+++ CCode/addition.c
@@ -8,3 +8,6 @@ int sum3(int x,int y,int z)
 {
 	return x+y+z;
 }
+
+int sum4(int x,int y,int z,int a) 
+	return x+y+z+a;
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -4,6 +4,9 @@ package learnJava;
 class Hello {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		helloWord();
+	}
+	public void helloWord() {
+	
 	}
 }
