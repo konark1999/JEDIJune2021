d6cd7ac80e8d2d2d36e7c42455c4fb7afc7d8621
Konark Garg <kitugarg.199@gmail.com>
2021-06-17 11:36:52 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,11 +1,13 @@
 // hello world program
 #include<stdio.h>
 void helloWorld();
-
+void greeting();
 int main() {
 	helloWorld();
 }
-
+void greeting() {
+	printf("good morning");
+}
 void helloWorld() {
 	printf("hello world");
         printf("awesome world");
