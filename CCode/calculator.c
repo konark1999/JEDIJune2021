94f7b424e079b26f38732e33d49156f4ad6b9134
konark1999 <kitugarg.199@gmail.com>
2021-06-20 22:48:48 +0530

    addition feature added

--- CCode/calculator.c
+++ CCode/calculator.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<limits.h>
+int addition(int a,int b) {
+	return a+b;
+}
 int main() {
 	int a,b;
 	scanf("%d %d",&a,&b);
+	int sum=addition(a,b);
+	printf("%d", sum);
 }
