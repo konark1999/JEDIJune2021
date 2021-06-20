ecf878ea5d632298a7a172f4d4a9d7d59364aefc
konark1999 <kitugarg.199@gmail.com>
2021-06-20 22:50:30 +0530

    subtraction feature added

--- CCode/calculator.c
+++ CCode/calculator.c
@@ -4,9 +4,14 @@
 int addition(int a,int b) {
 	return a+b;
 }
+int subtraction(int a,int b) {
+	return a-b;
+}
 int main() {
 	int a,b;
 	scanf("%d %d",&a,&b);
 	int sum=addition(a,b);
-	printf("%d", sum);
+	printf("sum is %d", sum);
+	int diff=subtraction(a,b);
+	printf("difference is %d",diff);
 }
